683a57851e4504b6a69cbdbc0283b21138c78c8b
Juuso Murto <juusomurto@kamk.fi>
2021-04-04 22:23:15 +0300

    animated burger menu

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const navSlide = () => {
+
+
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
@@ -12,5 +14,23 @@ const navSlide = () => {
         console.log(index / 7);
     });
 }
+
+const burger = document.querySelector('.burger');
+
+const burgerAnim = () => {
+    const line1 = document.getElementById('line1')
+    const line2 = document.getElementById('line2')
+    const line3 = document.getElementById('line3')
+    line1.classList.toggle('new-class1')
+    line2.classList.toggle('new-class2')
+    line3.classList.toggle('new-class3')
+
+}
+
+burger.addEventListener('click', () => {
+    burgerAnim();
+})
+
+
 
 navSlide();
